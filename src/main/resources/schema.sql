772d2a96bba14d152ec4631ffdb78acac5fbe5f2
Luis Criado San Miguel <psicolcsm@gmail.com>
2025-12-18 15:27:23 +0100

    Initial Spring Boot project setup

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prices (
+  brand_id     INTEGER NOT NULL,
+  product_id   INTEGER NOT NULL,
+  price_list   INTEGER NOT NULL,
+  start_date   TIMESTAMP NOT NULL,
+  end_date     TIMESTAMP NOT NULL,
+  priority     INTEGER NOT NULL,
+  price        DECIMAL(10,2) NOT NULL,
+  curr         VARCHAR(3) NOT NULL,
+  PRIMARY KEY (brand_id, product_id, price_list)
+);
